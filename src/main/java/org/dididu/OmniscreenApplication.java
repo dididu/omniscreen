13907a568e7cc58d3ecd0289659d03b8e35fa549
Dmitry Trofimov <dmitry.trofimov@gmail.com>
2016-11-18 23:20:25 +0000

    Change class visibility

--- src/main/java/org/dididu/OmniscreenApplication.java
+++ src/main/java/org/dididu/OmniscreenApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-class OmniscreenApplication {
+public class OmniscreenApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OmniscreenApplication.class, args);
