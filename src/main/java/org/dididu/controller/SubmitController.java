3fcbe44b886ef85e8a62b2587ba0e1cc7079a391
Dmitry Trofimov <dmitry.trofimov@gmail.com>
2016-11-19 21:42:20 +0000

    Add URL decode for body

--- src/main/java/org/dididu/controller/SubmitController.java
+++ src/main/java/org/dididu/controller/SubmitController.java
@@ -1,8 +1,12 @@
 package org.dididu.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 @RestController
 public class SubmitController {
@@ -11,7 +15,13 @@ public class SubmitController {
     private SimpMessagingTemplate template;
 
     @RequestMapping(value = "/widget", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void submitWidget(@RequestBody String widget) {
-        template.convertAndSend("/topic/widgets", widget);
+        try {
+            String decodedHtml = URLDecoder.decode(widget, "utf-8");
+            template.convertAndSend("/topic/widgets", decodedHtml);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
     }
 }
